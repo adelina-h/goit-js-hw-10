76a0aa7a85bfe453b1e9b5396317c446a6f4f9b9
Adelina-hub <adelina.macarascu@gmail.com>
2024-11-23 12:11:32 +0200

    hw done

---
+++ src/cat-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_zw1HIZKoBfbK6dLWCTJvA8n8QV0hyMp6sL4HSlfN32d4eEABbxTr7117b8H8jqKo';
+
+export const fetchBreeds = () => {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.data)
+    .catch(error => Promise.reject(error));
+};
+
+export const fetchCatByBreed = breedId => {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => response.data)
+    .catch(error => Promise.reject(error));
+};
